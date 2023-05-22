9dc223c0ef41132f140e336192d046651d2a6459
Confrontend <hamed.fatehi.66@gmail.com>
2023-05-22 21:34:22 +0200

    Handle firebase token and first steps towards mf

--- src/chat-room/chat-room.styled.tsx
+++ src/chat-room/chat-room.styled.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { Box, TextField } from "@mui/material";
+import { Box } from "@mui/material";
 
 const v = {
   messageInput: {
--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,18 @@ import { GlobalStyle } from "./index.styled.tsx";
 import { firebaseConfig } from "./config/firebase.config.ts";
 import { initializeFirebase } from "@Confrontend/chatly";
 
-console.log(import.meta.env.VITE_FIREBASE_API_KEY);
+const firebaseToken = localStorage.getItem("firebase_token") || ""
 
-initializeFirebase(firebaseConfig);
+console.log({ firebaseToken });
+
+if (!firebaseToken) {
+  throw new Error("Firebase token not found");
+}
+
+initializeFirebase(
+  firebaseConfig,
+  firebaseToken
+);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
