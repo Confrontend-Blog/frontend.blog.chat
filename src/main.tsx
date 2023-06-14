f39c0284cd1260aa8aed6b9f9c24e0dd80eb2bbd
Confrontend <hamed.fatehi.66@gmail.com>
2023-06-14 21:05:14 +0200

    firebase setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ChatRoom from "./chat-room/chat-room.tsx";
-import { GlobalStyle } from "./index.styled.tsx";
-import { firebaseConfig } from "./config/firebase.config.ts";
+import ChatRoom from "./chat-room/chat-room";
+import { GlobalStyle } from "./index.styled";
+import { firebaseConfig } from "./config/firebase.config";
 import { initializeFirebase } from "@Confrontend/chatly";
 
-console.log(import.meta.env.VITE_FIREBASE_API_KEY);
+try {
+  const firebaseToken = localStorage.getItem("firebase_token") || "";
 
-initializeFirebase(firebaseConfig);
+  console.log({ firebaseToken });
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <GlobalStyle />
-    <ChatRoom />
-  </React.StrictMode>
-);
+  if (!firebaseToken) {
+    throw new Error("Firebase token not found");
+  }
+
+  initializeFirebase(firebaseConfig, firebaseToken);
+
+  ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+      <GlobalStyle />
+      <ChatRoom />
+    </React.StrictMode>
+  );
+} catch (error) {
+  console.log(error);
+}
